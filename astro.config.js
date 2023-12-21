9d3701ed9c82a568419c7182795f0b7fbb21dca9
Daniil Savitskii <danildaniel121@gmail.com>
2023-12-22 02:01:09 +0400

    updated config

--- astro.config.js
+++ astro.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
+/** @param {import('astro').AstroConfig} */
 export default defineConfig({
-	integrations: [],
+	outDir: "./my-custom-build-directory",
+	cacheDir: "./.cache",
 });
