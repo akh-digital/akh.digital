2212bdda32bb294f13f0f01fabe4fc5014902125
Daniil Savitskii <danildaniel121@gmail.com>
2024-01-19 20:29:31 +0400

    add brand and projects section to home

--- astro.config.js
+++ astro.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import yaml from "@rollup/plugin-yaml";
 
 /** @param {import('astro').AstroConfig} */
 export default defineConfig({
 	outDir: "./my-custom-build-directory",
 	cacheDir: "./.cache",
+	vite: {
+		plugins: [yaml()],
+	},
 });
