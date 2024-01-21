4f84d51d2daa0ba61ad0efe9904f039d3cc5e694
Daniil Savitskii <danildaniel121@gmail.com>
2024-01-21 13:56:44 +0400

    moved to squoosh img service

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import yaml from "@rollup/plugin-yaml";
 
 /** @param {import('astro').AstroConfig} */
 export default defineConfig({
 	outDir: "./dist",
 	cacheDir: "./.cache",
+	image: {
+		service: squooshImageService(),
+	},
 	vite: {
 		plugins: [yaml()],
 	},
