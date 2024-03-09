9be7c6f71d7a3ac5a2bc5492e6dc8d445ac872dc
Daniil Savitskii <danildaniel121@gmail.com>
2024-03-09 04:28:40 +0400

    finished about page

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,17 +1,13 @@
 import AboutYaml from "../data/about.yml?raw";
 import ProjectYaml from "../data/projects.yml?raw";
-import {
-	AboutYaml as AboutYamlType,
-	ProjectYaml as ProjectYamlType,
-} from "../files.d.ts";
 import { parseYAML } from "confbox";
 
-/** @returns AboutYamlType */
+/** @returns {import('../files.js').AboutYaml} */
 export function about() {
 	return parseYAML(AboutYaml);
 }
 
-/** @returns ProjectYaml */
+/** @returns {import('../files.js').ProjectYaml}} */
 export function project() {
 	return parseYAML(ProjectYaml);
 }
